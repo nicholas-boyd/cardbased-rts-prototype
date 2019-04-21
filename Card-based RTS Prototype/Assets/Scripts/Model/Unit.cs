ec962e94be74345139813730d1f0e3cbc4e49ccb
nicholas-boyd <nicholas.randall.boyd@gmail.com>
2019-04-21 12:31:20 -0700

    Movement and Pathfinding
    Added Animation Utilities from concept, and the initial components for Movement and pathfinding

---
+++ Card-based RTS Prototype/Assets/Scripts/Model/Unit.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Unit : MonoBehaviour
+{
+    //public UnitRecipe recipe;
+    public Tile tile { get; protected set; }
+    //public AbilityDeck deck;
+    //public AbilityDeck discards;
+    //public List<Card> hand;
+    public int handSize = 4;
+    public Directions dir;
+    public bool acting;
+    public bool beingHealed;
+    public bool dying;
+    public int DeckCount;
+    public int flashCount;
+    public ParticleSystem death;
+    //EasingControl ec;
+    //Inventory inventory;
+
+    public const string EnemyDiedNotification = "ENEMY_DIED_NOTIFICATION";
+
+    void Start()
+    {
+        acting = false;
+        dying = false;
+        flashCount = 4;
+        Transform inv = this.transform.Find("Inventory");
+        if (inv == null)
+        {
+            GameObject i = new GameObject("Inventory");
+            i.transform.parent = this.transform;
+            inv = i.transform;
+        }
+        //inventory = inv.gameObject.AddComponent<Inventory>();
+    }
+
+    void OnEnable()
+    {
+        //this.AddObserver(OnHit, DamageAbilityEffect.DamageDealtNotification);
+        //this.AddObserver(OnHeal, HealAbilityEffect.UnitHealedNotification);
+        //this.AddObserver(OnDeath, Stats.DeathNotification, GetComponent<Stats>());
+    }
+
+    void OnDisable()
+    {
+        //this.RemoveObserver(OnHit, DamageAbilityEffect.DamageDealtNotification);
+        //this.RemoveObserver(OnHeal, HealAbilityEffect.UnitHealedNotification);
+        //this.RemoveObserver(OnDeath, Stats.DeathNotification, GetComponent<Stats>());
+    }
+
+    void OnHit(object sender, object target)
+    {
+        //Info<Unit, Unit, int> info = target as Info<Unit, Unit, int>;
+        //if (info.arg1.Equals(this))
+          //  StartCoroutine(Flash(Color.red));
+    }
+
+    void OnHeal(object sender, object target)
+    {
+        //Info<Unit, Unit, int> info = target as Info<Unit, Unit, int>;
+        //if (info.arg1.Equals(this))
+          //  StartCoroutine(Flash(Color.green));
+    }
+
+    void OnDeath(object sender, object target)
+    {
+        /*if (this != null)
+        {
+            if (GetComponent<Stats>()[StatTypes.HP] != 0)
+            {
+                Debug.Log("False death report");
+            }
+            else
+            {
+                StartCoroutine(Flash(Color.clear));
+                StartCoroutine(Die());
+            }
+        }*/
+    }
+
+    /*IEnumerator Die()
+    {
+        if (GetComponent<Alliance>().type == Alliances.Enemy)
+            this.PostNotification(EnemyDiedNotification, this);
+        ParticleSystem dying = Instantiate(death);
+        dying.transform.position = tile.center;
+        ec = gameObject.AddComponent<EasingControl>();
+        ec.duration = 0.2f;
+        ec.equation = EasingEquations.EaseInOutQuad;
+        ec.endBehaviour = EasingControl.EndBehaviour.Constant;
+        ec.updateEvent += OnUpdateEvent;
+        while (dying.isPlaying || ec.IsPlaying)
+            yield return null;
+        Destroy(this.gameObject);
+        this.tile.content = null;
+    }*/
+
+    void OnUpdateEvent(object sender, EventArgs e)
+    {
+        //GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, ec.currentValue);
+    }
+
+    IEnumerator Flash(Color color)
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        for (int i = 0; i < flashCount; i++)
+        {
+            sprite.color = color;
+            yield return new WaitForSeconds(0.06f);
+            sprite.color = Color.white;
+            yield return new WaitForSeconds(0.06f);
+        }
+    }
+
+    void Update()
+    {
+        //DeckCount = deck.Count;
+    }
+
+    /*public PlayerData GetPlayerData()
+    {
+        PlayerData ret = new PlayerData();
+        ret.recipe = this.recipe.name;
+
+        Card[] copy = deck.deck.ToArray();
+        string[] cards = new string[copy.Length];
+        for (int i = 0; i < copy.Length; i++)
+        {
+            Card c = copy[i];
+            if (c != null)
+            {
+                cards[i] = c.GetName();
+                Debug.Log(cards[i]);
+            }
+        }
+        ret.deck = cards;
+        ret.stats = this.GetComponent<Stats>().GetStatsData();
+        //ret.inventory = GetComponentInParent<GameController>()..GetData();
+
+        return ret;
+    }*/
+
+    public void Place(Tile target)
+    {
+        // Make sure old tile location is not still pointing to this unit
+        if (tile != null && tile.content == gameObject)
+            tile.content = null;
+
+        // Link unit and tile references
+        tile = target;
+
+        if (target != null)
+            target.content = gameObject;
+    }
+
+    public void Match()
+    {
+        transform.localPosition = new Vector3(tile.center.x, tile.center.y + GetComponent<SpriteRenderer>().bounds.size.y / 2, tile.center.z);
+        transform.localEulerAngles = dir.ToEuler();
+    }
+}
+
+[Serializable]
+public class PlayerData
+{
+    public string recipe;
+    public string[] deck;
+    //public StatsData stats;
+    //public InventoryData inventory;
+}
---
+++ Card-based RTS Prototype/Assets/Scripts/ViewModel/Movement/EnemyWalkMovement.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class EnemyWalkMovement : Movement
+{
+    public const string ReserveTileNotification = "ENEMY_ON_TILE";
+    public const string UnreserveTilesNotification = "ENEMY_ON_TILE";
+    protected List<Tile> unmoveableTiles;
+
+    void OnEnable()
+    {
+        this.AddObserver(AddUnmoveableTile, EnemyWalkMovement.ReserveTileNotification);
+        //this.AddObserver(AddUnmoveableTile, AIPlayerController.ReserveTileNotification);
+        this.AddObserver(RemoveUnmoveableTiles, EnemyWalkMovement.UnreserveTilesNotification);
+        //this.AddObserver(RemoveUnmoveableTile, AIPlayerController.UnreserveTileNotification);
+        this.AddObserver(EnemyDeath, Unit.EnemyDiedNotification);
+    }
+
+    void OnDisable()
+    {
+        this.RemoveObserver(AddUnmoveableTile, EnemyWalkMovement.ReserveTileNotification);
+        //this.RemoveObserver(AddUnmoveableTile, AIPlayerController.ReserveTileNotification);
+        this.RemoveObserver(RemoveUnmoveableTiles, EnemyWalkMovement.UnreserveTilesNotification);
+        //this.RemoveObserver(RemoveUnmoveableTile, AIPlayerController.UnreserveTileNotification);
+        this.RemoveObserver(EnemyDeath, Unit.EnemyDiedNotification);
+    }
+
+    void AddUnmoveableTile(object sender, object args)
+    {
+        unmoveableTiles.Add(args as Tile);
+    }
+
+    void RemoveUnmoveableTiles(object sender, object args)
+    {
+        List<Tile> tiles = args as List<Tile>;
+        if (tiles != null)
+            foreach (Tile t in tiles)
+                if (unmoveableTiles.Contains(t))
+                    unmoveableTiles.Remove(t);
+    }
+
+    void RemoveUnmoveableTile(object sender, object args)
+    {
+        Tile tile = args as Tile;
+        if (tile != null)
+            if (unmoveableTiles.Contains(tile))
+                unmoveableTiles.Remove(tile);
+    }
+
+    void EnemyDeath(object sender, object args)
+    {
+        if (unmoveableTiles.Contains((args as Unit).tile))
+            unmoveableTiles.Remove((args as Unit).tile);
+    }
+
+    void Start()
+    {
+        unmoveableTiles = new List<Tile>();
+    }
+
+    protected override bool ExpandSearch(Tile from, Tile to)
+    {
+        // Skip if the tile is occupied by an object
+        if (to.content != null)
+            return false;
+        // Skip if the tile is on the hero side
+        if (to.pos.x < 3)
+            return false;
+        // Skip if the tile is already reserved by another enemy
+        if (unmoveableTiles.Contains(to))
+            return false;
+        return base.ExpandSearch(from, to);
+    }
+
+    public override IEnumerator Traverse(Tile tile)
+    {
+        Tile startTile = unit.tile;
+        List<Tile> targs = GetTilesInRange(GetComponentInParent<BattleController>().board);
+        this.PostNotification(MovementBegunNotification);
+        float speed = 1;// gameObject.GetComponentInParent<Stats>()[StatTypes.SPD];
+        if (speed > 0)
+        {
+            unit.Place(tile);
+            // Build a list of way points from the unit's 
+            // starting tile to the destination tile
+            List<Tile> targets = new List<Tile>();
+            while (tile != null)
+            {
+                targets.Insert(0, tile);
+                this.PostNotification(ReserveTileNotification, tile);
+                tile = tile.prev;
+            }
+            // Move to each way point in succession
+
+            for (int i = 1; i < targets.Count; ++i)
+            {
+                Tile from = targets[i - 1];
+                Tile to = targets[i];
+                Directions dir = from.GetDirection(to);
+                if (dir == Directions.West)
+                {
+                    dir = Directions.North;
+                }
+                else if (dir == Directions.East)
+                {
+                    dir = Directions.South;
+                }
+                else if (dir == Directions.North)
+                {
+                    dir = Directions.South;
+                }
+                if (unit.dir != dir)
+                {
+                    yield return StartCoroutine(Flip(dir));
+                }
+                yield return StartCoroutine(Walk(to, speed));
+            }
+            yield return StartCoroutine(Flip(Directions.South));
+            this.PostNotification(UnreserveTilesNotification, targets);
+        }
+        this.PostNotification(MovementCompleteNotification);
+        yield return null;
+    }
+
+    IEnumerator Walk(Tile target, float speed)
+    {
+        Vector3 center = new Vector3(target.center.x, target.center.y + GetComponent<SpriteRenderer>().bounds.size.y / 2, target.center.z);
+        Tweener tweener = transform.MoveTo(center, speed, EasingEquations.Linear);
+        while (tweener != null)
+            yield return null;
+    }
+}
