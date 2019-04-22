4b9e8ae1a1d94d8037595d494d9dd17e6af0ec6d
nicholas-boyd <nicholas.randall.boyd@gmail.com>
2019-04-21 19:25:11 -0700

    Items
    Added Equippable and Consumable items with optional buy/sell prices. Created Feature class to describe Modifiers attached to Equippables.

---
+++ Card-based RTS Prototype/Assets/Scripts/Enums/EquipSlots.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Flags]
+public enum EquipSlots
+{
+    None = 0,
+    Hands = 1 << 0,
+    Head = 1 << 1,
+    UpperBody = 1 << 2,
+    LowerBody = 1 << 3,
+    Feet = 1 << 4
+}
---
+++ Card-based RTS Prototype/Assets/Scripts/ViewModel/Features/StatModifierFeature.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+public class StatModifierFeature : Feature
+{
+    #region Fields / Properties
+    public StatTypes type;
+    public float amount;
+    Stats stats
+    {
+        get
+        {
+            return _target.GetComponentInParent<Stats>();
+        }
+    }
+    #endregion
+    #region Protected
+    protected override void OnApply()
+    {
+        stats.AddValue(type, amount, false);
+    }
+
+    protected override void OnRemove()
+    {
+        stats.AddValue(type, amount*-1, false);
+    }
+    #endregion
+}
